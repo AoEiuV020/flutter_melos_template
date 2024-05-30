e61555ada9c763c69489a6d737d2ff9c6d0eb21f
AoEiuV020 <aoeiuv020@gmail.com>
2024-05-31 01:52:11 +0800

    get app创建支持直接覆盖根项目

--- script/app_get_create.sh
+++ script/app_get_create.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 . "$(dirname $0)/env.sh"
 
-# 检查并创建目录
-if [ ! -d "$apps_dir" ]; then
-    echo "Directory $apps_dir does not exist. Creating..."
-    mkdir -p "$apps_dir"
-fi
-cd "$apps_dir"
-expect <<EOF
-spawn sh -c "get create project:\"$app_name\""
+name=$1
+# name 传入了包名就在apps创建包，否则创建修改根项目，
+createProject() {
+    expect <<EOF
+spawn sh -c "get create project:\"$1\""
 expect "Select which type of project you want to create ?"
 send "1\r"
 expect "What is your company's domain?"
@@ -26,5 +23,25 @@ send "1\r"
 expect eof
 exit
 EOF
-echo 'include: package:flutter_lints/flutter.yaml' > "$app_name"/analysis_options.yaml
-cat "$ROOT"/analysis_options.yaml >> "$app_name"/analysis_options.yaml
+}
+if [ -z "$name" ] || [ "$name" = "." ]; then
+    cd "$ROOT"
+    rm -f pubspec.yaml README.md
+    mv .gitignore .gitignore.bak
+    createProject .
+    dart pub add dev:melos
+    cat .gitignore.bak >>.gitignore
+    rm -f .gitignore.bak
+else
+    # 检查并创建目录
+    if [ ! -d "$apps_dir" ]; then
+        echo "Directory $apps_dir does not exist. Creating..."
+        mkdir -p "$apps_dir"
+    fi
+    cd "$apps_dir"
+    createProject "$name"
+    cd "$name"
+fi
+echo 'include: package:flutter_lints/flutter.yaml' >analysis_options.yaml
+cat "$script_dir"/analyzer_custom.yaml >>analysis_options.yaml
+"$script_dir"/prepare.sh
