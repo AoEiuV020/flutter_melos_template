2c7ce8981d3bca3690bf71525efa791c6345b807
AoEiuV020 <aoeiuv020@gmail.com>
2025-01-03 21:43:41 +0800

    修env.sh
    用了bash语法， 在mac正常， 在linux就不行了，

--- script/env.sh
+++ script/env.sh
@@ -3,7 +3,7 @@ set -e
 script_dir=$(cd $(dirname $0);pwd)
 ROOT=$(dirname "$script_dir")
 project_name=$(basename "$ROOT")
-app_name=${project_name//_/}
+app_name=$(echo "$project_name" | tr -d '_')
 apps_dir="$ROOT"/apps
 packages_dir="$ROOT"/packages
 organization=com.aoeiuv020
