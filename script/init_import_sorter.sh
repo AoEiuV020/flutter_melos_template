d0d37f293a50484ac0b10e3fe6957179fb334a26
AoEiuV020 <aoeiuv020@gmail.com>
2026-02-02 01:25:22 +0800

    修import_sorter
    停更了，新的继任者一直没上，不知道为什么， 只能改用fork版，

--- script/init_import_sorter.sh
+++ script/init_import_sorter.sh
@@ -4,5 +4,5 @@
 # 检查是否安装了 import_sorter
 if ! dart pub global list |grep import_sorter > /dev/null; then
     echo "import_sorter is not installed. Installing..."
-    dart pub global activate import_sorter
+    dart pub global activate --source git https://github.com/myConsciousness/import_sorter.git --git-ref fix/monorepo-pub-workspaces
 fi
