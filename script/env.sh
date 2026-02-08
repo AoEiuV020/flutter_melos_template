fb133fb8e2bf36eba5a5886dc7a046fbf114da7b
AoEiuV020 <aoeiuv020@gmail.com>
2026-02-08 23:24:37 +0800

    删除不再需要的sh脚本
    改用skill中的dart脚本，

--- script/env.sh
+++ script/env.sh
@@ -8,10 +8,3 @@ apps_dir="$ROOT"/apps
 packages_dir="$ROOT"/packages
 organization=com.aoeiuv020
 
-# 更新 workspace 配置的函数
-update_workspace() {
-  local root_path="${1:-$ROOT}"
-  local module_path="${2:-}"
-  
-  dart "$script_dir/update_workspace.dart" "$root_path" "$module_path"
-}
